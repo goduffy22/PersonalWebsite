15fe5cbc6cc066117a43b1d0b9ef721b90757b42
Gareth ODuffy <goduffy22@gmail.com>
2018-09-05 15:54:54 +0100

    First Draft: General structure

--- home.js
+++ home.js
@@ -1,7 +1,23 @@
 $(function(){
     $("#countVisits").click(function(){
+
         var count = parseInt($(".badge").text());
         count++;
         $(".badge").text(count);
-    })
-})
+    });
+
+    $("#navbarSupportedContent a").on('click', function(event) {
+
+        if (this.hash !== "") {
+            event.preventDefault();
+            var hash = this.hash;
+            $('html, body').animate({
+               scrollTop: $(hash).offset().top
+            }, 800, function(){
+                window.location.hash = "#" + hash;
+            });
+        }
+        $("title").text(hash);
+
+    });
+});
